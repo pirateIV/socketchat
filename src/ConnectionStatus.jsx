f51463b2ddc7a2b573ddad2118319edb683a794f
pirateIV <benabolade@gmail.com>
2024-07-25 19:54:26 +0100

    done with part ui

--- src/ConnectionStatus.jsx
+++ src/ConnectionStatus.jsx
@@ -1,6 +1,16 @@
 const ConnectionStatus = ({ isConnected }) => {
   return (
-    <p id="connection-status">status: {isConnected ? "online" : "offline"}</p>
+    <div className="flex items-center gap-2 pt-1.5">
+      <div
+        id="online-status"
+        className={`w-3 h-3 rounded-full ${
+          isConnected ? "bg-green-500" : "bg-red-500"
+        }`}
+      ></div>
+      <span className="opacity-40 text-xs text-right" id="connection-status">
+        {isConnected ? "online" : "offline"}
+      </span>
+    </div>
   );
 };
 
--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,11 +1,28 @@
 import { socket } from "./socket";
+import ConnectionStatus from "./ConnectionStatus";
+import { Link, NavLink } from "react-router-dom";
 
-const Sidebar = () => {
+const Sidebar = ({ isConnected }) => {
   return (
-    <div className="min-h-screen py-5 bg-blue-950 text-white w-60">
-      <h1 className="text-3xl my-5 px-5">Sidebar</h1>
+    <aside className="h-full w-64 bg-gradient-to-b from-purple-950/90 to-purple-950 border-r-2 border-purple-500 text-white">
+      <header className="text-3xl p-3 h-24">
+        <div i-socket-logo="true"></div>
+      </header>
 
-    </div>
+      <div className="flex flex-col h-[calc(100vh-6rem)] gap-0 overflow-auto">
+        {socket.id ? (
+          <Link to={socket.id} className="block bg-blue-200/15 py-4 px-2.5">
+            <div className="flex items-center gap-3">
+              <p className="font-bold font-gentium text-sm">
+                {socket.id?.substring(0, 14)}{" "}
+                <span className="text-xs opacity-65">(Yourself)</span>
+              </p>
+            </div>
+            <ConnectionStatus isConnected={isConnected} />
+          </Link>
+        ) : null}
+      </div>
+    </aside>
   );
 };
 
