24070929cafb04052aba6b267c31227c6c8907c5
pirateIV <benabolade@gmail.com>
2024-07-25 23:37:15 +0100

    remove socket conn. error

--- src/socket.js
+++ src/socket.js
@@ -3,9 +3,7 @@ import { io } from "socket.io-client";
 const URL =
   process.env.NODE_ENV === "production" ? false : "http://localhost:3000";
 
-export const socket = io("https://socketchat-xe13.onrender.com", {
-  transports: ["websockets"],
-});
+export const socket = io(URL);
 socket.onAny((event, ...args) => {
   console.log(event, args);
 });
