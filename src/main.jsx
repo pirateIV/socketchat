023f2365268a007b4f13436164c01beac6d9f8bd
pirateIV <benabolade@gmail.com>
2024-08-24 19:33:16 +0100

    init (redux toolkit)

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,12 @@
-import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import ReactDOM from "react-dom/client";
 
 import App from "./App";
+import { store } from "@/app/store";
 import "./index.css";
 
-createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+);
